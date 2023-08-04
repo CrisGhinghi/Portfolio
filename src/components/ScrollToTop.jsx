ca0915f54c26fa5eb6f6ea4a5c45dcfdf6871fdb
Cris <ghinghiloschicristina@gmail.com>
2023-08-04 18:27:32 +0200

    Create ScrollToTop.jsx

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import { FaArrowCircleUp } from 'react-icons/fa';
+
+const ScrollToTop = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 500) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", toggleVisibility);
+        return () => window.removeEventListener("scroll", toggleVisibility);
+    }, []);
+
+    return (
+        <div className="fixed bottom-5 right-5 cursor-pointer">
+            {isVisible && (
+                <div onClick={scrollToTop} className="text-4xl text-gary-900 hover:text-rose-600">
+                    <FaArrowCircleUp />
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default ScrollToTop;
