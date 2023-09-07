2756fb064f710bd2bf46b6a9ff86560e9b90bfb4
Cris <ghinghiloschicristina@gmail.com>
2023-09-07 15:21:07 +0200

    Update portfolioData.js

--- src/components/data/portfolioData.js
+++ src/components/data/portfolioData.js
@@ -3,38 +3,46 @@ import likePhoto from '../../assets/portfolio/likePhoto.png'
 import randomizeColors from "../../assets/portfolio/randomizeColors.png"
 import testimonials from '../../assets/portfolio/testimonials.png'
 import dreamCity from '../../assets/portfolio/dreamCity.png'
+import meditation from '../../assets/portfolio/meditation.png'
 
 export const portfolioData = [
     {
         id: 1,
+        image: meditation,
+        title: 'Meditation',
+        github: 'https://github.com/CrisGhinghi/S2I-React-Meditation',
+        demo: 'https://letgomeditation.netlify.app',
+    },
+    {
+        id: 2,
         image: dreamCity,
         title: 'Dream City',
         github: 'https://github.com/CrisGhinghi/S2I-JS--Advanced',
         demo: 'https://yourdreamcity.netlify.app',
     },
     {
-        id: 2,
+        id: 3,
         image: counter,
         title: 'Counter',
         github: 'https://github.com/CrisGhinghi/S2I-JS--Basics',
         demo: 'https://counter-app-crisghinghi.netlify.app',
     },
     {
-        id: 3,
+        id: 4,
         image: randomizeColors,
         title: 'Random Colors',
         github: 'https://github.com/CrisGhinghi/React--01-Randomize-colors',
         demo: 'https://randomizecolors.netlify.app',
     },
     {
-        id: 4,
+        id: 5,
         image: likePhoto,
         title: 'Like Photo',
         github: 'https://github.com/CrisGhinghi/React--02-Like-my-photo',
         demo: 'https://likemyphotoapp.netlify.app',
     },
     {
-        id: 5,
+        id: 6,
         image: testimonials,
         title: 'Testimonials',
         github: 'https://github.com/CrisGhinghi/React--03-Testimonials',
