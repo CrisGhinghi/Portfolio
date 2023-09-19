693afe70f6cdebc2fbab2ef639238b6d21f8cf0d
Cris <ghinghiloschicristina@gmail.com>
2023-09-19 17:58:57 +0200

    Update portfolioData.js

--- src/components/data/portfolioData.js
+++ src/components/data/portfolioData.js
@@ -1,51 +1,51 @@
 import counter from '../../assets/portfolio/counter.png'
 import likePhoto from '../../assets/portfolio/likePhoto.png'
 import randomizeColors from "../../assets/portfolio/randomizeColors.png"
-import testimonials from '../../assets/portfolio/testimonials.png'
 import dreamCity from '../../assets/portfolio/dreamCity.png'
 import meditation from '../../assets/portfolio/meditation.png'
+import climatic from '../../assets/portfolio/climatic.png'
 
 export const portfolioData = [
     {
         id: 1,
+        image: climatic,
+        title: 'Climatic',
+        github: 'https://github.com/CrisGhinghi/S2I--FinalProject-GlobalWarming',
+        demo: 'https://climatic-globalwarming.netlify.app',
+    },
+    {
+        id: 2,
         image: meditation,
         title: 'Meditation',
         github: 'https://github.com/CrisGhinghi/S2I-React-Meditation',
         demo: 'https://letgomeditation.netlify.app',
     },
     {
-        id: 2,
+        id: 3,
         image: dreamCity,
         title: 'Dream City',
         github: 'https://github.com/CrisGhinghi/S2I-JS--Advanced',
         demo: 'https://yourdreamcity.netlify.app',
     },
     {
-        id: 3,
+        id: 4,
         image: counter,
         title: 'Counter',
         github: 'https://github.com/CrisGhinghi/S2I-JS--Basics',
         demo: 'https://counter-app-crisghinghi.netlify.app',
     },
     {
-        id: 4,
+        id: 5,
         image: randomizeColors,
         title: 'Random Colors',
         github: 'https://github.com/CrisGhinghi/React--01-Randomize-colors',
         demo: 'https://randomizecolors.netlify.app',
     },
     {
-        id: 5,
+        id: 6,
         image: likePhoto,
         title: 'Like Photo',
         github: 'https://github.com/CrisGhinghi/React--02-Like-my-photo',
         demo: 'https://likemyphotoapp.netlify.app',
-    },
-    {
-        id: 6,
-        image: testimonials,
-        title: 'Testimonials',
-        github: 'https://github.com/CrisGhinghi/React--03-Testimonials',
-        demo: 'https://testimonials-webapp.netlify.app',
     },
 ]
